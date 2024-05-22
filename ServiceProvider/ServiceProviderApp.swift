98eb298a807fe32e0caa6fa9f95754e1729d4789
datBatmanCoder <jonakandel@gmail.com>
2024-05-22 11:59:49 +0200

    Initial Commit

---
+++ ServiceProvider/ServiceProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceProviderApp.swift
+//  ServiceProvider
+//
+//  Created by Jonathan Kandel on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ServiceProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
